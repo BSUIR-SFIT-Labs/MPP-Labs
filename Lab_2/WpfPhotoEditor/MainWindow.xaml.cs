7f1c8dea403d3f7323d7876e3e6b1f7dd400f748
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-10-18 17:14:51 +0300

    feat(wpf): add tools to the form for photo editing

--- Lab_2/WpfPhotoEditor/MainWindow.xaml.cs
+++ Lab_2/WpfPhotoEditor/MainWindow.xaml.cs
@@ -22,5 +22,65 @@ namespace WpfPhotoEditor
         {
             throw new System.NotImplementedException();
         }
+
+        private void BtnRotateToSpecDegreesRight_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ApplyChanges_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnRotateTo90DegreesRight_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnRotateTo90DegreesLeft_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnHorizontalFlip_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnVerticalFlip_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SHue_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SBrightness_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SContrast_OnValueChangedContrast_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SSaturation_OnValueChangedContrast_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnApplyFilter_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
