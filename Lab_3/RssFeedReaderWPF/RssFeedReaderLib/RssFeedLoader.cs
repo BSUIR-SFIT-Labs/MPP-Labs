55ed0172ec32f410d24d6845cd790eba49f951c5
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-19 20:21:19 +0300

    feat(lab-3): implement the function of polling RSS sources

---
+++ Lab_3/RssFeedReaderWPF/RssFeedReaderLib/RssFeedLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace RssFeedReaderLib
+{
+    public class RssFeedLoader
+    {
+        public static Rss20FeedFormatter LoadFeedFromUrl(string feedUrl)
+        {
+            var rssFeedFormatter = new Rss20FeedFormatter();
+
+            using var xmlReader = XmlReader.Create(feedUrl);
+            rssFeedFormatter.ReadFrom(xmlReader);
+
+            return rssFeedFormatter;
+        }
+    }
+}
