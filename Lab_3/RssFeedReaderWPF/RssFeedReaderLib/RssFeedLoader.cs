a86c7acd501bd6912c576612a2cdc4afdea5cdd9
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-19 20:35:23 +0300

    feat(lab-3): implement feed filtering by tags

--- Lab_3/RssFeedReaderWPF/RssFeedReaderLib/RssFeedLoader.cs
+++ Lab_3/RssFeedReaderWPF/RssFeedReaderLib/RssFeedLoader.cs
@@ -1,4 +1,6 @@
-﻿using System.ServiceModel.Syndication;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Xml;
 
 namespace RssFeedReaderLib
@@ -13,6 +15,39 @@ namespace RssFeedReaderLib
             rssFeedFormatter.ReadFrom(xmlReader);
 
             return rssFeedFormatter;
+        }
+
+        public static Rss20FeedFormatter FilterFeedByTags(Rss20FeedFormatter rssFeedFormatter,
+            IEnumerable<string> tags)
+        {
+            var feed = new SyndicationFeed
+            {
+                Title = new TextSyndicationContent("Filtered feed")
+            };
+
+            var resultRssFeedFormatter = new Rss20FeedFormatter(feed);
+            resultRssFeedFormatter.Feed.Items = FilterItemsInFeedByTags(rssFeedFormatter.Feed.Items, tags);
+
+            return resultRssFeedFormatter;
+        }
+
+        private static IEnumerable<SyndicationItem> FilterItemsInFeedByTags(IEnumerable<SyndicationItem> feedItems,
+            IEnumerable<string> tags)
+        {
+            var filteredListOfRssItems = new List<SyndicationItem>();
+
+            var listOfTags = tags.ToList();
+
+            if (listOfTags.Any())
+            {
+                filteredListOfRssItems.AddRange(feedItems.Where(rssFeedItem =>
+                    listOfTags.Any(tag =>
+                        rssFeedItem.Summary.Text.ToUpper().Contains(tag.ToUpper()))));
+
+                return filteredListOfRssItems;
+            }
+
+            return feedItems.ToList();
         }
     }
 }
