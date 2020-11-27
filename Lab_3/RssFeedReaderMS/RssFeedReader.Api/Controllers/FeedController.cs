b53a0e5499f124410244c97562373ac5ae915790
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-27 08:34:59 +0300

    feat(lab-3): implement a microservice for receiving RSS feed

---
+++ Lab_3/RssFeedReaderMS/RssFeedReader.Api/Controllers/FeedController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RssFeedReader.Api.Models;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace RssFeedReader.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FeedController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetFeed(string feedUrl)
+        {
+            var formatter = new Rss20FeedFormatter();
+
+            using var reader = XmlReader.Create(feedUrl);
+            formatter.ReadFrom(reader);
+
+            var feed = formatter.Feed.Items.Select(feedItem =>
+                new Feed
+                {
+                    Title = feedItem.Title.Text,
+                    Link = feedItem.Links.First().Uri.ToString()
+                }).ToList();
+
+            return Ok(feed);
+        }
+    }
+}
---
+++ Lab_3/RssFeedReaderMS/RssFeedReader.Api/Models/Feed.cs
@@ -0,0 +1,9 @@
+﻿namespace RssFeedReader.Api.Models
+{
+    public class Feed
+    {
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+    }
+}
